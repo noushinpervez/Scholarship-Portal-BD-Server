f3a42f54ddc87bf1ae8b53cb482c2330c8d2c29d
noushinpervez <134955206+noushinpervez@users.noreply.github.com>
2024-06-21 02:37:14 +0600

    Implemented api for fetching top scholarships sorted by fees and date

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+// config
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9crls8f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const scholarshipCollection = client.db("scholarshipPortalDB").collection("scholarships");
+
+        app.get("/top-scholarships", async (req, res) => {
+            // Sort scholarships by post date (newest first) and then by application fees (lowest first)
+            const scholarships = await scholarshipCollection.find()
+                .sort({ application_fees: 1, post_date: -1 })
+                .toArray();
+
+            res.send(scholarships);
+        });
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Server is running...');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
